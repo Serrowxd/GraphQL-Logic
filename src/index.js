34275591fefb6aac5398f9fdc869616d8159585f
Kevin Jolley <serrowpls@gmail.com>
2020-09-30 12:59:05 -0500

    pub/sub with votes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 const { PrismaClient } = require("@prisma/client");
 const { GraphQLServer } = require("graphql-yoga");
+const { PubSub } = require("graphql-yoga");
 
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
+
+const pubsub = new PubSub();
 
 const prisma = new PrismaClient();
 
@@ -22,6 +29,7 @@ const server = new GraphQLServer({
     return {
       ...requests,
       prisma,
+      pubsub,
     };
   },
 });
